5a7f255e2501850caf39b0e0ca2f4f92054248d7
Muhammad Syarifudin <muhammad_syarif@students.itb.ac.id>
2021-01-20 01:51:32 +0700

    Exploring the Service Provider

---
+++ tests/Commands/TryPackageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Syarifid\TryPackage\Tests\Commands;
+
+use Syarifid\TryPackage\Tests\TestCase;
+/**
+ * 
+ */
+class TryPackageTest extends TestCase
+{
+	/** @test */
+	public function try_package_command_works()
+	{
+		$this->artisan('try-package')->assertExitCode(0);
+	}
+}
