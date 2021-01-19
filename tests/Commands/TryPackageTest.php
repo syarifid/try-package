c924ed5b24c889262615b2231c4ffcabec617289
syarifid <syarifid@users.noreply.github.com>
2021-01-19 18:52:23 +0000

    Fix styling

--- tests/Commands/TryPackageTest.php
+++ tests/Commands/TryPackageTest.php
@@ -3,14 +3,15 @@
 namespace Syarifid\TryPackage\Tests\Commands;
 
 use Syarifid\TryPackage\Tests\TestCase;
+
 /**
- * 
+ *
  */
 class TryPackageTest extends TestCase
 {
-	/** @test */
-	public function try_package_command_works()
-	{
-		$this->artisan('try-package')->assertExitCode(0);
-	}
+    /** @test */
+    public function try_package_command_works()
+    {
+        $this->artisan('try-package')->assertExitCode(0);
+    }
 }
